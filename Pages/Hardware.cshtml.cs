cc8de95812f986153be942b48bfa7d29209ca092
synnedch <dennisschaeppi@outlook.com>
2024-08-23 04:59:34 +0200

    Hardware Overview implemented

---
+++ Pages/Hardware.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace HardwareMgmt.Pages;
+
+public class HardwareModel : PageModel
+{
+    private readonly ILogger<HardwareModel> _logger;
+    private readonly CosmosDbService _cosmosDbService;
+    public IEnumerable<HardwareItem> HardwareItems { get; private set; }
+    public HardwareModel(ILogger<HardwareModel> logger, CosmosDbService cosmosDbService)
+    {
+        _logger = logger;
+        _cosmosDbService = cosmosDbService;
+    }
+
+    public async Task OnGetAsync()
+    {
+      HardwareItems = await _cosmosDbService.GetHardwareItemsAsync();
+    }
+}
+
